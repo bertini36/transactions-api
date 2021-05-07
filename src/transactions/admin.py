988dc79b6a786c053ae83816efbb28bcdacb4834
Alberto Pou <albertopouquiros@gmail.com>
2021-05-07 16:09:54 +0200

    First version upload csv

--- src/transactions/admin.py
+++ src/transactions/admin.py
@@ -1,10 +1,32 @@
+import io
+from csv import reader
+
 from django.contrib import admin
+from django.contrib import messages
+from .actions import create_transactions
+from .models import Transaction, UploadedFile
 
-from .models import Transaction
 
-
+@admin.register(Transaction)
 class TransactionAdmin(admin.ModelAdmin):
     list_display = ('date', 'account', 'amount')
 
 
-admin.site.register(Transaction, TransactionAdmin)
+@admin.register(UploadedFile)
+class UploadedFileAdmin(admin.ModelAdmin):
+    fields = ('file',)
+    list_display = ('filename',)
+
+    def save_model(self, request, obj, form, change):
+        obj.filename = obj.file.name
+        raw_transactions = io.StringIO(obj.file.read().decode('utf-8'))
+        csv_reader = reader(raw_transactions)
+        next(csv_reader)  # Ignore header
+        raw_transactions = list(csv_reader)
+        transactions = create_transactions(raw_transactions)
+        messages.add_message(
+            request,
+            messages.SUCCESS,
+            f'{len(transactions)} transactions have been registered'
+        )
+        obj.save()
