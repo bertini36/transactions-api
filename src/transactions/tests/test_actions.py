be11dad31e58f60827fe6881819aa2e585b30ced
Alberto Pou <albertopouquiros@gmail.com>
2021-05-07 17:32:08 +0200

    Get balances by account action tests

--- src/transactions/tests/test_actions.py
+++ src/transactions/tests/test_actions.py
@@ -1,6 +1,10 @@
+from datetime import date
+
+from django.conf import settings
 from django.test import TestCase
 
-from ..actions import create_transactions
+from ..actions import create_transactions, get_balances_by_account
+from ..models import Transaction
 
 
 class CreateTransactionsActionTest(TestCase):
@@ -27,3 +31,46 @@ class CreateTransactionsActionTest(TestCase):
         transactions = create_transactions(raw_transactions)
 
         self.assertEqual(len(transactions), 0)
+
+
+class GetBalancesByAccountActionTest(TestCase):
+    def test_balance_is_calculated_right_for_one_account(self):
+        year = settings.DEFAULT_YEAR
+        Transaction.objects.bulk_create(
+            [
+                Transaction(date=date(year, 1, 1), account=1, amount=10),
+                Transaction(date=date(year, 1, 1), account=1, amount=-10),
+            ]
+        )
+
+        balances_by_account = get_balances_by_account(year)
+
+        self.assertEqual(balances_by_account[0]['balance'], 0)
+
+    def test_multiple_balance_are_returned_when_multiple_accounts_registered(
+        self,
+    ):  # noqa
+        year = settings.DEFAULT_YEAR
+        Transaction.objects.bulk_create(
+            [
+                Transaction(date=date(year, 1, 1), account=1, amount=10),
+                Transaction(date=date(year, 1, 1), account=2, amount=20),
+            ]
+        )
+
+        balances_by_account = get_balances_by_account(year)
+
+        self.assertEqual(len(balances_by_account), 2)
+
+    def test_no_balances_returned_when_no_transactions_for_a_given_year(self):
+        year = settings.DEFAULT_YEAR
+        Transaction.objects.bulk_create(
+            [
+                Transaction(date=date(year, 1, 1), account=1, amount=10),
+                Transaction(date=date(year, 1, 1), account=2, amount=20),
+            ]
+        )
+
+        balances_by_account = get_balances_by_account(2020)
+
+        self.assertEqual(len(balances_by_account), 0)
