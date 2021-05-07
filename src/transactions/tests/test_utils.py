f083dc4b0e4355490e522731fd3c4f49792ad2c5
Alberto Pou <albertopouquiros@gmail.com>
2021-05-07 21:05:40 +0200

    Test delete_key function

--- src/transactions/tests/test_utils.py
+++ src/transactions/tests/test_utils.py
@@ -1,4 +1,4 @@
-from ..utils import get_raw_transactions_from_read_obj
+from ..utils import get_raw_transactions_from_read_obj, delete_key
 
 
 def test_get_raw_transactions_from_csv_file():
@@ -6,3 +6,29 @@ def test_get_raw_transactions_from_csv_file():
         raw_transactions = get_raw_transactions_from_read_obj(read_obj)
 
     assert len(raw_transactions) == 9  # File has 10 rows but header is ignored
+
+
+def test_delete_key_of_a_dict():
+    data = [{'a': 1, 'b': 2}]
+
+    data = delete_key(data, 'a')
+
+    assert set(data[0].keys()) == {'b'}
+
+
+def test_delete_key_of_multiples_dicts():
+    data = [{'a': 1, 'b': 2}, {'a': 3, 'b': 4}]
+
+    data = delete_key(data, 'a')
+
+    assert len(data) == 2
+    assert set(data[0].keys()) == {'b'}
+    assert set(data[1].keys()) == {'b'}
+
+
+def test_no_delete_when_key_is_not_in_dict():
+    data = [{'a': 1, 'b': 2}]
+
+    data = delete_key(data, 'c')
+
+    assert set(data[0].keys()) == {'a', 'b'}
