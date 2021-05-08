c04668c0f3d906216d3656da8871d3310771bd67
Alberto Pou <albertopouquiros@gmail.com>
2021-05-08 11:35:02 +0200

    Fix typing

--- src/transactions/queries.py
+++ src/transactions/queries.py
@@ -1,19 +1,21 @@
-from typing import List
+from typing import Iterable
 
 from .models import Transaction
 from .utils import delete_key
 
 
-def get_annual_balances(account: int = None, year: int = None) -> List[dict]:
+def get_annual_balances(
+    account: int = None, year: int = None
+) -> Iterable[dict]:
     balances = Transaction.objects.annual_balance(account=account, year=year)
-    return list(balances)
+    return balances
 
 
 def get_monthly_balances(
     account: int = None, year: int = None, month: int = None
-) -> List[dict]:
+) -> Iterable[dict]:
     balances = Transaction.objects.monthly_balance(
         account=account, year=year, month=month
     )
     balances = delete_key(balances, 'date__month')
-    return list(balances)
+    return balances
